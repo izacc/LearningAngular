029beeed1c2c827dbeeb456fdebcb512a3c10a3a
Izacc Lucas <Izacc.casey-lucas01@stclairconnect.ca>
2020-11-06 12:16:50 -0500

    finished promise
    Completed our promise to pop up error message and changed our content-list page to show a section for everything, so we can see our new entries

--- src/app/create-content/create-content.component.ts
+++ src/app/create-content/create-content.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {Content} from '../helper-files/content-interface';
 import {ContentListComponent} from '../content-list/content-list.component';
+import {error} from '@angular/compiler/src/util';
 
 @Component({
   selector: 'app-create-content',
@@ -17,7 +18,10 @@ export class CreateContentComponent implements OnInit {
   public type: string;
   public body: string;
   public form: FormGroup;
-  constructor() { }
+  public invalidForm: boolean;
+  constructor() {
+    this.invalidForm = false;
+  }
 
   ngOnInit(): void {
     this.form = new FormGroup({
@@ -35,23 +39,32 @@ export class CreateContentComponent implements OnInit {
   }
   onSubmit(): void {
     const submitPromise = new Promise((success, fail) => {
-      if (this.form.valid) {
-        const contentItem: Content = {
-          id: this.form.value.contentId,
-          author: this.form.value.contentAuthor,
-          imgUrl: this.form.value.contentImgUrl,
-          title: this.form.value.contentTitle,
-          tags: this.form.value.contentTags,
-          type: this.form.value.contentType,
-          body: this.form.value.contentBody
-        };
-        success(`The Content ${this.form.value.contentTitle} has been successfully added`);
-        ContentListComponent.contentList.push(contentItem);
-        this.Clear();
-      } else {
-        fail('Content could not be added');
-      }
-    });
+    if (this.form.valid) {
+      const contentItem: Content = {
+        id: this.form.value.contentId,
+        author: this.form.value.contentAuthor,
+        imgUrl: this.form.value.contentImgUrl,
+        title: this.form.value.contentTitle,
+        tags: this.form.value.contentTags,
+        type: this.form.value.contentType,
+        body: this.form.value.contentBody
+      };
+      success(`The Content ${this.form.value.contentTitle} has been successfully added`);
+      this.invalidForm = false;
+      ContentListComponent.contentList.push(contentItem);
+      this.Clear();
+    } else {
+      console.log(this.form.get('contentId').status);
+      console.log(this.form.get('contentAuthor').status);
+      console.log(this.form.get('contentImgUrl').status);
+      console.log(this.form.get('contentTitle').status);
+      console.log(this.form.get('contentTags').status);
+      console.log(this.form.get('contentType').status);
+      console.log(this.form.get('contentBody').status);
+      this.invalidForm = true;
+      fail('Content could not be added');
+    }
+  });
     submitPromise.then(
       (success) => console.log(success),
       (fail) => console.log(fail)
